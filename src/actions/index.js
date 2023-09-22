e737990ccb6a1e3e52984624e3ef46b17aded894
Sterling Sanders <sterlingsanders723@gmail.com>
2023-09-22 14:13:57 -0700

    All Done:)

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,35 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEM_PLUS = "MEM_PLUS";
+export const MEM_R = "MEM_R";
+export const MEM_CLEAR = "MEM_CLEAR";
 
 export const addOne = () => {
-    return({type:ADD_ONE});
+    return ({ type: ADD_ONE });
 }
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
+    return ({ type: APPLY_NUMBER, payload: number });
+}
+
+export const changeOperation = (operation) => {
+    return ({ type: CHANGE_OPERATION, payload: operation })
+}
+
+export const clearDisplay = () => {
+    return ({ type: CLEAR_DISPLAY })
+}
+
+export const memPlus = () => {
+    return ({ type: MEM_PLUS })
+}
+
+export const memR = () => {
+    return ({ type: MEM_R })
+}
+
+export const memClear = () => {
+    return ({ type: MEM_CLEAR })
 }
